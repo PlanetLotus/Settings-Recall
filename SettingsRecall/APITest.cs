41177393e1c289523b53395682cb8b47ec66bdf2
Matt <matt@cygwin.com>
2013-12-22 20:54:17 -0800

    Begin writing unittests.
    Added the NUnit library to the project.
    Only the unittest class has been set up; the tests have not been
    written yet. I ran into an error when trying to turn the project into a
    DLL and don't feel like researching the issue at the moment.

---
+++ SettingsRecall/APITest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SettingsRecall {
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class APITest {
+        SQLiteDatabase db;
+        SQLiteAPI testAPI;
+
+        [TestFixtureSetUp]
+        public void Init() {
+            // Create a testing database
+            db = new SQLiteDatabase("unittest.db");
+
+            // Create some test data directly in the db, without the API
+            Dictionary<string, Dictionary<string, string>> dict = new Dictionary<string, Dictionary<string, string>>();
+            dict.Add("xp", new Dictionary<string, string>() { { "1", "xp/path/to/file1.txt" }, { "2", "xp/path/to/file2.txt" } });
+            dict.Add("vista", new Dictionary<string,string>() { {"1", "vista/path/to/file1.txt"}});
+            string json_paths = JsonConvert.SerializeObject(dict);
+
+            Dictionary<string, string> insert = new Dictionary<string, string>();
+            insert.Add("Name", "testprogram1");
+            insert.Add("Paths", json_paths);
+            insert.Add("Description", "testdescription1");
+            db.Insert("Program", insert);   // Should we wrap this in try/catch?
+
+            // Link the API to the testing database
+            testAPI = new SQLiteAPI("unittest.db");
+        }
+
+        [TestFixtureTearDown]
+        public void Cleanup() {
+            // Delete all data in the db
+            db.ClearDB();
+        }
+    }
+}
