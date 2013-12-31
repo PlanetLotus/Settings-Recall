fbdc66d9c33a2dc9d44ca4de9f8105c500c16b85
Matt <matt@cygwin.com>
2013-12-30 23:10:40 -0800

    Update Init and begin writing tests.
    Updated Init to match the new db schema. Began writing test for
    AddProgram but it doesn't check for success yet.

--- SettingsRecall/APITest.cs
+++ SettingsRecall/APITest.cs
@@ -12,32 +12,47 @@ namespace SettingsRecall {
     public class APITest {
         SQLiteDatabase db;
         SQLiteAPI testAPI;
+        string db_file = "unittest.db";
 
         [TestFixtureSetUp]
         public void Init() {
             // Create a testing database
-            db = new SQLiteDatabase("unittest.db");
+            db = new SQLiteDatabase(db_file);
 
             // Create some test data directly in the db, without the API
-            Dictionary<string, Dictionary<string, string>> dict = new Dictionary<string, Dictionary<string, string>>();
-            dict.Add("xp", new Dictionary<string, string>() { { "1", "xp/path/to/file1.txt" }, { "2", "xp/path/to/file2.txt" } });
-            dict.Add("vista", new Dictionary<string,string>() { {"1", "vista/path/to/file1.txt"}});
-            string json_paths = JsonConvert.SerializeObject(dict);
+            List<string> paths = new List<string>();
+            paths.Add("path/to/\"quoted filename.txt\"");
+            paths.Add("path/to/file2.txt");
+            paths.Add("path/to/file3.txt");
+            string json_paths = JsonConvert.SerializeObject(paths);
 
             Dictionary<string, string> insert = new Dictionary<string, string>();
             insert.Add("Name", "testprogram1");
+            insert.Add("Version", "1.0");
+            insert.Add("OS", "XP");
+            insert.Add("IsPermanent", "0");
             insert.Add("Paths", json_paths);
             insert.Add("Description", "testdescription1");
             db.Insert("Program", insert);   // Should we wrap this in try/catch?
 
             // Link the API to the testing database
-            testAPI = new SQLiteAPI("unittest.db");
+            testAPI = new SQLiteAPI(db_file);
         }
 
         [TestFixtureTearDown]
         public void Cleanup() {
             // Delete all data in the db
             db.ClearDB();
+        }
+
+        [Test]
+        public void TestAddProgram() {
+            // This test depends on assuming GetProgramNameList works properly
+
+            // Add a program name to the db
+            testAPI.AddProgram("testAddProgram1");
+
+            // Make sure it was added by calling GetProgramNameList
         }
     }
 }
