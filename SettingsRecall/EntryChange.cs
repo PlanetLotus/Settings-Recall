0502d51d0285415f915bf82b787a8dd518d7954e
Thomas Fritchman <tcfritchman@gmail.com>
2014-01-04 22:22:57 -0800

    Add EntryChange class
    This class will be used by the edit window in a list of program changes.

---
+++ SettingsRecall/EntryChange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsRecall
+{
+    /// <summary>
+    /// Provides required information to perform a change to a program entry.
+    /// </summary>
+    class EntryChange
+    {
+        public string entry_type { get; set; } // The type of modification. Add, Edit, or Delete
+        public ProgramEntry entry { get; set; } // the ProgramEntry data to be used in the modification.
+    }
+}
