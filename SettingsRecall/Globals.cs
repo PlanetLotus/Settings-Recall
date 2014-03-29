da24b143bad8b8e78b9c33d9896b874cebcda950
Matt <matt@cygwin.com>
2014-03-29 12:23:27 -0700

    Remove unused helper.
    Also, it was created before we had a Helpers class and was in the wrong
    class...

--- SettingsRecall/Globals.cs
+++ SettingsRecall/Globals.cs
@@ -14,23 +14,5 @@ namespace SettingsRecall
         public static SQLiteAPI sqlite_api;
         public static string load_save_location; // Where backup files are saved
         public static string dbLocation = "../../test.db";
-
-        // Global Helpers
-
-        /// <summary>
-        /// Convert a number represented by a string to an int.
-        /// </summary>
-        /// <param name="numStr">String to be converted</param>
-        /// <returns>Int representation of string.</returns>
-        public static int StrToInt(string numStr) {
-           // This function is simply to keep the error checking out of normal code to reduce clutter
-           int numInt;
-           try { numInt = Convert.ToInt32(numStr); }
-           catch(FormatException) { return -1; }
-           catch(OverflowException) { return -1; }
-
-           return numInt;
-       }
-
     }
 }
