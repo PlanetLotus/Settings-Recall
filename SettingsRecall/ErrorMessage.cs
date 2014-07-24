cd803fdda0734d689917b250d0885782409db2bf
Matt Messmer <staind2523@gmail.com>
2014-07-24 14:22:08 -0700

    Improve ErrorMessage functionality.
    To solve the problem of only having one instance per window, I modified
    the class so that a window can have one private ErrorMessage instance
    and can have errors added/removed from it at any time. I also maintain a
    reference to the window from the ErrorMessage instance so that the
    window reference doesn't have to be passed in more than once.
    
    I still need to support:
    - Closing the error message
    - Removing individual error labels from the error message
    - (Potentially) Showing the error message somewhere other than the
    bottom of a window

--- SettingsRecall/ErrorMessage.cs
+++ SettingsRecall/ErrorMessage.cs
@@ -4,28 +4,54 @@ using System.Windows.Media;
 
 namespace SettingsRecall {
     class ErrorMessage : Border {
-        public ErrorMessage(string bodyText, Window parentWindow = null) {
+        public ErrorMessage(Window parentWindow) {
+            window = parentWindow;
+
             CornerRadius = new CornerRadius(4);
-            BrushConverter converter = new BrushConverter();
+            Margin = new Thickness(0, 5, 0, 0);
             BorderBrush = (Brush)converter.ConvertFromString("#ebccd1");
             Background = (Brush)converter.ConvertFromString("#f2dede");
-            Brush textBrush = (Brush)converter.ConvertFromString("#a94442");
+            stackPanel = new StackPanel();
+            Child = stackPanel;
+        }
 
-            Label body = new Label { Content = bodyText, Foreground = textBrush };
+        public ErrorMessage(string bodyText, Window parentWindow) {
+            window = parentWindow;
 
-            StackPanel stackPanel = new StackPanel { Children = { body } };
+            CornerRadius = new CornerRadius(4);
+            BorderBrush = (Brush)converter.ConvertFromString("#ebccd1");
+            Background = (Brush)converter.ConvertFromString("#f2dede");
+            stackPanel = new StackPanel();
             Child = stackPanel;
 
-            if (parentWindow != null)
-                ShowInWindow(parentWindow);
+            AddErrorLabel(bodyText);
+            ShowInWindow();
         }
 
-        public void ShowInWindow(Window window) {
-            ((Panel)window.Content).Children.Add(this);
+        public void ShowInWindow() {
+            if (!((Panel)window.Content).Children.Contains(this))
+                ((Panel)window.Content).Children.Add(this);
         }
 
-        public void RemoveFromWindow(Window window) {
+        public void RemoveFromWindow() {
             ((Panel)window.Content).Children.Remove(this);
         }
+
+        public void AddErrorLabel(string errorText) {
+            foreach (Label label in stackPanel.Children) {
+                if (label.Content.ToString() == errorText)
+                    return;
+            }
+
+            Label errorLabel = new Label { Content = errorText, Foreground = textBrush };
+            stackPanel.Children.Add(errorLabel);
+
+            ShowInWindow();
+        }
+
+        private static BrushConverter converter = new BrushConverter();
+        private Brush textBrush = (Brush)converter.ConvertFromString("#a94442");
+        private StackPanel stackPanel;
+        private Window window;
     }
 }
