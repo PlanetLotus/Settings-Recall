4df04a28fd44c2acd966a695a97a6d48b1ee875b
Matt Messmer <staind2523@gmail.com>
2014-07-23 18:46:02 -0700

    Add ErrorMessage class.
    This is meant to replace ErrorMessageBox, since it's more user friendly
    to display an error inside the same window than create another dialog
    that they have to close. I'll phase out and eventually delete
    ErrorMessageBox soon.
    
    The idea behind ErrorMessage is to create a standard look for an error
    message. I plan on extending this as needed...it could include header
    text (optionally, anyway), it should be closeable by the user, etc.
    
    I need a way of easily keeping track of ErrorMessage instances in a
    window so that the window doesn't create multiples of them, and so it
    can destroy it when necessary.

---
+++ SettingsRecall/ErrorMessage.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SettingsRecall {
+    class ErrorMessage : Border {
+        public ErrorMessage(string bodyText, Window parentWindow = null) {
+            CornerRadius = new CornerRadius(4);
+            BrushConverter converter = new BrushConverter();
+            BorderBrush = (Brush)converter.ConvertFromString("#ebccd1");
+            Background = (Brush)converter.ConvertFromString("#f2dede");
+            Brush textBrush = (Brush)converter.ConvertFromString("#a94442");
+
+            Label body = new Label { Content = bodyText, Foreground = textBrush };
+
+            StackPanel stackPanel = new StackPanel { Children = { body } };
+            Child = stackPanel;
+
+            if (parentWindow != null)
+                ShowInWindow(parentWindow);
+        }
+
+        public void ShowInWindow(Window window) {
+            ((Panel)window.Content).Children.Add(this);
+        }
+
+        public void RemoveFromWindow(Window window) {
+            ((Panel)window.Content).Children.Remove(this);
+        }
+    }
+}
