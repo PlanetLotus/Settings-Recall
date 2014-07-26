6ad331010b31cc7276f6089cde0ceaac14d2a70b
Matt Messmer <staind2523@gmail.com>
2014-07-26 00:10:21 -0700

    Add extension method.

---
+++ SettingsRecall/Utility/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SettingsRecall.Utility {
+    public static class Extensions {
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source) {
+            return new HashSet<T>(source);
+        }
+    }
+}
